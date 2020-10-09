38027d201cae1b400385e915bc355aa7e46c9ff3
José Man <34019072+chikenwing24@users.noreply.github.com>
2020-10-08 18:31:08 -0600

    Add files via upload
    Portafolio web Octubre 8 del 2020

---
+++ js/main.js
@@ -0,0 +1,61 @@
+  TweenMax.to(".overlaypink h1", 2, {
+    delay: 1,
+    opacity: 0,
+    y: -60,
+    ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlaypink", 2, {
+    delay: 2,
+    left: "-100%",
+    ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlayrosa h1", 2, {
+  delay: 1,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlayrosa", 2, {
+  delay: 2,
+  left: "-100%",
+  ease: Expo.easeInOut
+})
+TweenMax.to(".overlaygreen h1", 2, {
+  delay: 1,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlaygreen", 2, {
+  delay: 2,
+  left: "-100%",
+  ease: Expo.easeInOut
+})
+TweenMax.to(".overlaypiel h1", 2, {
+  delay: 1,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlaypiel", 2, {
+  delay: 2,
+  left: "-100%",
+  ease: Expo.easeInOut
+})
+TweenMax.to(".overlayblue h1", 2, {
+  delay: 1,
+  opacity: 0,
+  y: -60,
+  ease: Expo.easeInOut
+})
+
+TweenMax.to(".overlayblue", 2, {
+  delay: 2,
+  left: "-100%",
+  ease: Expo.easeInOut
+})
